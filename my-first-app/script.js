ee090fbc70c41b139613649e8c1ab3f1c27c4ba9
chanmin <kimchanmin1@gmail.com>
2024-10-27 19:24:53 +0900

    chore: container

--- my-first-app/script.js
+++ my-first-app/script.js
@@ -1,4 +1,14 @@
 "use strict";
 const message = "";
 
-window.onload = () => {};
+const d = document;
+const startButton = d.querySelector(".button-start");
+const titleContainer = d.querySelector(".title-container");
+const formContainer = d.querySelector(".form-container");
+const displayContainer = d.querySelector(".display-container");
+
+window.onload = () => {
+  titleContainer.style.opacity = "1";
+};
+
+startButton.addEventListener("click", () => {});
